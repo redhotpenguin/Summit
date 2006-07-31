84bc6fc78033c41d98348b372dabaca38f9e6c5a
fred <fred@11b7a2cd-b219-0410-8b85-994f1aaba3df>
2006-07-31 15:35:50 +0000

    - Forgot to commit the lib the other day.
    git-svn-id: https://www.redhotpenguin.com/svn/summit/trunk@3 11b7a2cd-b219-0410-8b85-994f1aaba3df

---
+++ lib/Summit.pm
@@ -0,0 +1,9 @@
+package Summit;
+
+use strict;
+use warnings FATAL => 'all';
+use English qw(no_match);
+
+
+
+1;
---
+++ lib/Summit/DB.pm
@@ -0,0 +1,35 @@
+package Summit::DB;
+
+use strict;
+use warnings FATAL => 'all';
+
+=item C<params>
+
+Returns an array reference for DBI connection parameters
+
+ $params_ref = SL::DB->params;
+ DBI->connect(@{$params_ref});
+
+=cut
+
+sub params {
+    my ( $class, %args ) = @_;
+    my $db_user = $args{db_user} || 'summit';
+    my $db_pass = $args{db_pass} || '';
+    my $db_name = $args{db_name} || 'summit';
+    my $db_host = $args{db_host} || 'localhost';
+    my $db_options = {
+        RaiseError         => 1,
+        PrintError         => 1,
+        AutoCommit         => 0,
+        FetchHashKeyName   => 'NAME_lc',
+        ShowErrorStatement => 1,
+        ChopBlanks         => 1,
+    };
+    my $dsn = qq/dbi:Pg:dbname='$db_name';host=$db_host/;
+
+    my @connect = ( $dsn, $db_user, $db_pass, $db_options );
+	return wantarray ? @connect : \@connect
+}
+
+1;
